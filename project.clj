e0be69e8a9ee41458176f9ad7c4328ce729b36a2
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-22 13:04:26 +0800

    use symlinks during development

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [org.clojure/clojurescript "0.0-3297"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
 
-                 [infinitelives.pixi/infinitelives.pixi "0.1.0-SNAPSHOT"]
+                 ;[infinitelives.pixi/infinitelives.pixi "0.1.0-SNAPSHOT"]
                  ]
 
   :plugins [[lein-cljsbuild "1.0.5"]
