83669b20afed4f83e8ed6e187a134af00d6a28ae
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-22 23:22:20 +0800

    get running working in all directions

--- src/ludumdare33/core.cljs
+++ src/ludumdare33/core.cljs
@@ -7,6 +7,7 @@
      [infinitelives.utils.console :refer [log]]
      [infinitelives.pixi.texture :as texture]
      [infinitelives.utils.math :as math]
+     [infinitelives.utils.vec2 :as vec2]
      [cljs.core.async :refer [<!]])
     (:require-macros
      [cljs.core.async.macros :refer [go]]
@@ -45,10 +46,23 @@
 
 (def player-run-anim-delay 7)
 
-(def player-frames
-  [[[128 8] [128 40] [128 72] [128 104]]
+#_ (def player-frames
+  [[[128 8] [128 32] [128 56] [128 80]]
    [[168 8] [168 40] [168 72] [168 104]]
    [[208 8] [208 40] [208 72] [208 104]]])
+
+(def player-frames
+  {
+   :left [[128 8] [128 32] [128 56] [128 80]]
+   :down-left [[160 8] [160 40] [160 72] [160 104]]
+   :down [[192 8] [192 40] [192 72] [192 104]]
+   :down-right [[224 8] [224 40] [224 72] [224 104]]
+   :right [[256 8] [256 32] [256 56] [256 80]]
+   :up-right [[288 8] [288 40] [288 72] [288 104]]
+   :up [[320 8] [320 40] [320 72] [320 104]]
+   :up-left [[352 8] [352 40] [352 72] [352 104]]
+
+})
 
 (def game-props
   {
@@ -128,12 +142,14 @@
          :fade-out 0.5))
 
     (let [spritesheet (resources/get-texture :sprites :nearest)
-          topleft (player-frames 0)
-          running (map
-                   #(texture/sub-texture spritesheet
-                                         %
-                                         [24 24])
-                   topleft)]
+          player-tex (into {}
+                           (for [[k v] player-frames]
+                             [k (doall (map
+                                        #(texture/sub-texture spritesheet
+                                                              %
+                                                              [24 24])
+                                        v))]))
+          ]
 
 
       (go (let [props (make-prop-texture-lookup game-props)]
@@ -145,7 +161,7 @@
                                                     :xhandle 0.5 :yhandle 1.0)))]
                                         ;(log "!" (last sprites))
               (macros/with-sprite canvas :world
-                [player (sprite/make-sprite (first running) :scale scale
+                [player (sprite/make-sprite (-> player-tex :left first) :scale scale
                                          :xhandle 0.5 :yhandle 0.9)]
 
                 (<! (resources/fadein player :duration 0.5))
@@ -155,14 +171,61 @@
                   (<! (events/wait-frames player-run-anim-delay))
                   (recur (inc n)))
 
-                (loop [[x y] [0 0] n 0]
-                  (sprite/set-pivot! (-> canvas :layer :world) (* 6 x) y)
-                  (sprite/set-pos! player (* 6 x) y)
-                  (sprite/set-texture! player (nth running (mod (int (* 0.18 n)) (count running))))
+                (loop [pos (vec2/zero)
+                       vel (vec2/vec2 -6 0)
+                       n 0]
+                  (let [next-pos (vec2/add pos vel)
+                        x (aget next-pos 0)
+                        y (aget next-pos 1)]
 
-                  (.sort (.-children (-> canvas :layer :world)) depth-compare )
-                  (<! (events/next-frame))
-                  (recur [(dec x) y] (inc n))
+                    (sprite/set-pivot! (-> canvas :layer :world)  x y)
+                    (sprite/set-pos! player x y)
+
+
+
+                    ;; set animation frame
+                    ;
+                    (let [
+                          pi Math/PI
+                          pi-on-4 (/ pi 4)
+                          pi-on-8 (/ pi 8)
+                          head (vec2/heading vel)
+                          section
+                          (int (/ (+ pi-on-8 head) pi-on-4))
+                          frame
+                          ([:right :down-right :down :down-left
+                            :left :up-left :up :up-right :right]
+                           section)
+
+
+                          ]
+                      (sprite/set-texture! player (nth
+                                                   (-> player-tex frame)
+                                                   (mod (int (* 0.02 n)) (-> player-tex frame count)))))
+
+                    (.sort (.-children (-> canvas :layer :world)) depth-compare )
+                    (<! (events/next-frame))
+                    (recur next-pos
+
+                           ;;cursor keys
+                           (let [x (if (events/is-pressed? :left)
+                                     -1 (if (events/is-pressed? :right)
+                                          1
+                                          0))
+                                 y (if (events/is-pressed? :up)
+                                     -1 (if (events/is-pressed? :down) 1 0))]
+                             (log (vec2/vec2 x y))
+                             (log (vec2/unit (vec2/vec2 x y)))
+                             (vec2/truncate (vec2/add
+                                             (if (and (= x 0) (= y 0))
+                                               (vec2/vec2 x y)
+                                               (vec2/scale (vec2/unit (vec2/vec2 x y))
+                                                           1.5))
+                                             (vec2/scale vel 0.98))
+                                            10
+                                            ))
+
+                           (+ n (vec2/magnitude vel))))
                   )
 
                 (<! (resources/fadeout player :duration 0.5)))
