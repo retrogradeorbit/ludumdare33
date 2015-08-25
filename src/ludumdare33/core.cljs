b5a567b0f90716f076c3a41636ec9313f693f2bc
Crispin Wellington <retrogradeorbit@gmail.com>
2015-08-25 18:54:55 +0800

    obsolete

--- src/ludumdare33/core.cljs
+++ src/ludumdare33/core.cljs
@@ -21,14 +21,6 @@
 (defonce fonts
   [(font/install-google-font-stylesheet! "http://fonts.googleapis.com/css?family=Amatic+SC")])
 
-
-(println "Edits to this text should show up in your developer console.")
-
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:text "Hello world!"}))
-
-
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
